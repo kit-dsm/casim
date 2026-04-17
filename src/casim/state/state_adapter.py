534995874fa7d8109debf9daa31a07f72bacf3f6
j4n1k <janik.bischoff@gmx.de>
2026-04-17 19:43:04 +0200

    renamed state snapshot to state adapter

--- src/casim/state/state_adapter.py
+++ src/casim/state/state_adapter.py
@@ -2,13 +2,14 @@ from pathlib import Path
 
 from hydra.utils import instantiate
 from omegaconf import DictConfig
+from ware_ops_algos.algorithms import AlgorithmSolution, SchedulingSolution
 from ware_ops_algos.domain_models import Resources, WarehouseInfoType, ResourceType, OrdersDomain, OrderType
 
 from casim.domain_objects.sim_domain import SimWarehouseDomain, DynamicInfo
 from casim.state import State
 
 
-def build_state_snapshots(cfg: DictConfig) -> dict:
+def build_state_adapters(cfg: DictConfig) -> dict:
     return {
         problem_key: instantiate(st_cfg,
                                  domain_cache_path=Path(cfg.cache_base))
@@ -16,15 +17,18 @@ def build_state_snapshots(cfg: DictConfig) -> dict:
     }
 
 
-class StateSnapshot:
+class StateAdapter:
     def __init__(self):
         pass
 
     def transform_state(self, state: State, problem: str):
         pass
 
+    def cleanup_state(self, state: State, solution: AlgorithmSolution):
+        pass
 
-class OBRSPSnapshot(StateSnapshot):
+
+class OBRSPAdapter(StateAdapter):
     def __init__(self, domain_cache_path):
         super().__init__()
         self.domain_cache_path = domain_cache_path
@@ -66,3 +70,55 @@ class OBRSPSnapshot(StateSnapshot):
         )
 
         return dynamic_information
+
+    def cleanup_state(self, state: State, solution: AlgorithmSolution):
+        pass
+
+
+class ORSPAdapter(StateAdapter):
+    def __init__(self, domain_cache_path):
+        super().__init__()
+        self.domain_cache_path = domain_cache_path
+        self.orders = None
+        self.selected_picker = None
+
+    def transform_state(self, state: State, problem: str):
+        layout = state.layout_manager.get_layout()
+        buffered_pls = state.order_manager.get_pick_list_buffer()
+
+        warehouse_info = DynamicInfo(
+            tpe=WarehouseInfoType.ONLINE,
+            time=state.current_time,
+            congestion_rate=None,
+            current_picker=None,
+            buffered_pick_lists=buffered_pls,
+            active_tours=None,
+            done=state.done_flag
+        )
+        resources = state.resource_manager.get_resources()
+        dynamic_resources_list = []
+        for r in resources.resources:
+            if not r.occupied:
+                dynamic_resources_list.append(r)
+
+        dynamic_resources = Resources(ResourceType.HUMAN, dynamic_resources_list)
+
+        dynamic_information = SimWarehouseDomain(
+            problem_class=problem,
+            objective="Distance",
+            layout=layout,
+            orders=OrdersDomain(tpe=OrderType.STANDARD, orders=[]),
+            resources=dynamic_resources,
+            articles=state.storage_manager.get_articles(),
+            storage=state.get_storage(),
+            dynamic_warehouse_info=warehouse_info
+        )
+
+        return dynamic_information
+
+    def cleanup_state(self, state: State, solution: SchedulingSolution):
+        pls = []
+        for j in solution.jobs:
+            pls.append(j.route.pick_list)
+        state.order_manager.clear_pick_list_buffer(pls)
+        print(f"Garbage Collected {len(pls)} batches")
